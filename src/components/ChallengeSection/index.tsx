ea95dc0599b3351440a234c7ff3cafcf27c24b82
villAsh <chavhanvilas02@gmail.com>
2025-04-13 13:47:28 +0530

    refactor layout and add ChallengeSection component

---
+++ src/components/ChallengeSection/index.tsx
@@ -0,0 +1,18 @@
+import data from "@/data/data.json";
+import ChallengeCard from "@/components/challengeCard";
+const ChallengeSection = () => {
+  return (
+    <section className="flex flex-col justify-center items-center gap-y-4 mt-8 pt-6 w-1/4 max-lg:w-full">
+      {data.map((item) => (
+        <ChallengeCard
+          key={item.id}
+          title={item.title}
+          id={item.id}
+          isDisabled={item.disabled}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default ChallengeSection;
