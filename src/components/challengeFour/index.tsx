9387814d473d3e9606ddf656bff35d1e4c1b4a67
villAsh <chavhanvilas02@gmail.com>
2025-04-16 20:08:00 +0530

    feat: refactor ChallengeFour component to use ToggleButton for improved readability

--- src/components/challengeFour/index.tsx
+++ src/components/challengeFour/index.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import ChallengeTitle from "../ChallengeTitle";
-import { AnimatePresence, motion } from "framer-motion";
+import ToggleButton from "./components/ToggleButton";
 
 const ChallengeFour = () => {
   const [isOn, setIsOn] = useState(false);
+
   return (
     <main className="flex flex-col justify-center items-center gap-y-4 h-[80vh]">
       <ChallengeTitle
@@ -12,59 +13,7 @@ const ChallengeFour = () => {
         title="Animated Toggles"
       />
       <div className="flex flex-col justify-start items-start gap-y-2 mt03">
-        <motion.div
-          animate
-          className="relative flex justify-start items-start bg-white p-1 rounded-full w-[30vw] h-20"
-        >
-          <motion.button
-            onClick={() => setIsOn(!isOn)}
-            animate={{
-              left: isOn ? "49%" : "1%",
-            }}
-            transition={{
-              duration: 0.3,
-            }}
-            className="absolute flex justify-center items-center bg-black p-1 rounded-full w-1/2 h-[90%] hover:cursor-pointer"
-          >
-            <AnimatePresence mode="wait">
-              {isOn && (
-                <motion.p
-                  key="premium"
-                  className="flex flex-col justify-center items-center"
-                  initial={{ x: 30, opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{
-                    duration: 0.2,
-                  }}
-                  exit={{
-                    x: 30,
-                    opacity: 0,
-                  }}
-                >
-                  <span className="font-semibold text-lg">Premium</span>
-                  <span className="font-medium text-sm">Monthly + Annual</span>
-                </motion.p>
-              )}
-              {!isOn && (
-                <motion.h6
-                  key="free"
-                  className="font-[600] text-lg"
-                  initial={{ x: -30, opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{
-                    duration: 0.2,
-                  }}
-                  exit={{
-                    x: -30,
-                    opacity: 0,
-                  }}
-                >
-                  Free
-                </motion.h6>
-              )}
-            </AnimatePresence>
-          </motion.button>
-        </motion.div>
+        <ToggleButton isOn={isOn} setIsOn={setIsOn} />
       </div>
     </main>
   );
